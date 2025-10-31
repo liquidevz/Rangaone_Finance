3382615b2c392a59e4b536bd5ccb11ec4260ad5e
AntiMatter <varunbhole@gmail.com>
2025-11-01 04:47:56 +0530

    Docker & VPS Conflicts with Youtube Iframe Embeds Fix

--- components/mobile-video-player.tsx
+++ components/mobile-video-player.tsx
@@ -7,10 +7,12 @@ export function MobileVideoPlayer({ youtubeId, title }: MobileVideoPlayerProps) 
   return (
     <div className="responsive-container">
       <iframe 
-        src={`https://www.youtube.com/embed/${youtubeId}`} 
+        src={`https://www.youtube.com/embed/${youtubeId}?enablejsapi=1&origin=${typeof window !== 'undefined' ? window.location.origin : ''}`}
         title={title}
         frameBorder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
+        referrerPolicy="strict-origin-when-cross-origin"
       />
     </div>
   )
